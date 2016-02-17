19c674a835e5124443a333fc7fee7301cbde4684
Phisan Sookkhee <phisan@Phisans-MacBook-Pro.local>
2016-02-17 16:05:12 +0700

    modified:   bash.sh 	new file:   countItems.py

--- bash.sh
+++ bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # ./dataprepare.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed'
-./reduceResult.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed/001.vector'
+# ./reduceResult.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed/001.vector'
+./countItems.py '/Users/phisan/Desktop/001.vector'
 
