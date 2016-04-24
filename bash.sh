93f5c508177b6fca736ed5c53050288bca488f07
Phisan Shukkhi <phisanshukkhi@Phisans-Mac-mini.local>
2016-04-24 15:23:48 +0700

    modified:   bash.sh 	modified:   createWordsListSymp.py

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./createWVHerb.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed'
-
+./createWordsListSymp.py '/Users/phisanshukkhi/Desktop/006.WordVectorHerb'
+# ./createWVHerb.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed'
 # ./createWordVectorHS.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed'
 # ./dataprepare.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed'
 # ./reduceResult.py '/Volumes/Phisan Segate/ข้อมูลวิจัย/ระบบสนับสนุนการตัดสินใจ/Processed/001.vector'
--- createWordsListSymp.py
+++ createWordsListSymp.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
 import csv
 import math
+from os import listdir
+from os.path import isfile, join
+from os import path
+from os import makedirs
 
 def readSymptoms():
     symptoms = []
@@ -10,7 +15,7 @@ def readSymptoms():
         symptoms.append(row[1].strip())
     return symptoms
 
-def main():
+def main(directory, filename, resultPath):
     # Read CSV
     symptoms = readSymptoms()
 
@@ -30,11 +35,9 @@ def main():
         wordsSymp[index][1] = 's'+str(index-1)
         index += 1
 
-    row_size = len(symptoms) + 3
     row_count = 1
-
-    filename = "336-ถั่วเหลือง.csv"
-    rows = csv.reader(open("/Users/phisan/Desktop/006.WordVectorHerb/"+filename))
+    print directory+"/"+filename
+    rows = csv.reader(open(directory+"/"+filename))
 
     totalDocument = 0
     for row in rows:
@@ -62,45 +65,56 @@ def main():
     row_count = 0
     for row in wordsSymp:
         if row_count > 0:
-            wordsSymp[row_count][4] = (float(row[2]) / float(feq_total_sym)) * 100
-            wordsSymp[row_count][5] = (float(row[3]) / float(totalDocument)) * 100
+            if feq_total_sym != 0:
+                wordsSymp[row_count][4] = (float(row[2]) / float(feq_total_sym)) * 100
+            if totalDocument != 0:
+                wordsSymp[row_count][5] = (float(row[3]) / float(totalDocument)) * 100
             if wordsSymp[row_count][2] != 0:
                 wordsSymp[row_count][6] = math.log10(totalDocument / wordsSymp[row_count][3])
         row_count += 1
 
-    print "Total Documents : ", totalDocument
-
     # Write result to CSV
-    rootPath = "/Users/phisan/Desktop"
-    destPath = rootPath+"/wordsSymp.csv"
+    destination_dir = resultPath+"/wordList"
+    if not path.exists(destination_dir):
+        makedirs(destination_dir)
+
+    destPath = destination_dir+"/"+filename
     with open(destPath, "wb") as f:
         writer = csv.writer(f)
         writer.writerows(wordsSymp)
 
     # Calculate TF-IDF for each term
-    destPath = "/Users/phisan/Desktop/"+filename
-    outfile = open(destPath, 'w')
+    destination_dir = resultPath+"/vectorTFIDF"
+    if not path.exists(destination_dir):
+        makedirs(destination_dir)
 
-    rows = csv.reader(open("/Users/phisan/Desktop/006.WordVectorHerb/"+filename))
+    destFile = destination_dir+"/"+filename
+    outfile = open(destFile, 'w')
+
+    rows = csv.reader(open(directory+"/"+filename))
     row_count = 0
-    new_row = []
+
     for row in rows:
+        new_row = []
         column_index = 0
         if row_count > 0:
             for term_feq in row:
                 wordList_index = column_index - 2
                 if 2 < column_index < len(symptoms) + 3:
-                    term_idf = wordsSymp[wordList_index][6]
-                    weight = float(term_feq) * float(term_idf)
-                    new_row.append(str(weight))
+                    if int(term_feq) != 0:
+                        term_idf = wordsSymp[wordList_index][6]
+                        weight = float(term_feq) * float(term_idf)
+                        weight = "{0:.2f}".format(weight)
+                        new_row.append(str(weight))
+                    else:
+                        new_row.append(term_feq)
                 else:
                     new_row.append(term_feq)
-
-                newline = ",".join(new_row)+"\n"
-                newline = newline.encode('utf-8')
-                outfile.write(newline)
-
                 column_index += 1
+
+            newline = ",".join(new_row)+"\n"
+            newline = newline.encode('utf-8')
+            outfile.write(newline)
         else:
             for header in row: new_row.append(header)
             newline = ",".join(new_row)+"\n"
@@ -111,9 +125,17 @@ def main():
 
     outfile.close()
 
-
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        directory = sys.argv[1]
+        resultPath = '/Users/phisanshukkhi/Desktop'
+
+        onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+        for filename in onlyfiles:
+            if filename != ".DS_Store":
+                main(directory, filename, resultPath)
+    else:
+        print "Please, Enter File Directory"
 
 
 
